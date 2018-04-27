904d68aa996bef157c491035dc632f11e3a636c1
Francisco Lima <franciscojml@gmail.com>
2018-04-27 15:29:39 -0300

    criacao da classe de servico de usuario

--- src/main/java/com/fjlima/helpdesk/api/repository/TicketRepository.java
+++ src/main/java/com/fjlima/helpdesk/api/repository/TicketRepository.java
@@ -1,8 +1,7 @@
 package com.fjlima.helpdesk.api.repository;
 
-import java.awt.print.Pageable;
-
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.fjlima.helpdesk.api.entity.Ticket;
---
+++ src/main/java/com/fjlima/helpdesk/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.fjlima.helpdesk.api.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.fjlima.helpdesk.api.entity.User;
+import com.fjlima.helpdesk.api.repository.UserRepository;
+import com.fjlima.helpdesk.api.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User findByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	@Override
+	public User createOrUpdate(User user) {
+		return userRepository.save(user);
+	}
+
+	@Override
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+
+	@Override
+	public void delete(String id) {
+		userRepository.deleteById(id);
+
+	}
+
+	@Override
+	public Page<User> findAll(int page, int count) {
+		Pageable pages = new PageRequest(page, count);
+		return userRepository.findAll(pages);
+	}
+
+}
