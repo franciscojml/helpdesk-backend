b59a85405bb7ef0d9b9ecbe274c1c459251eaaaf
Francisco Lima <franciscojml@gmail.com>
2018-04-27 15:34:11 -0300

    @transient na Entidade Ticket e Containing nos metodos TicketRepository

--- src/main/java/com/fjlima/helpdesk/api/entity/Ticket.java
+++ src/main/java/com/fjlima/helpdesk/api/entity/Ticket.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -37,6 +38,7 @@ public class Ticket implements Serializable {
 
 	private String image;
 
+	@Transient
 	private List<ChangeStatus> changes;
 
 	public Ticket() {
--- src/main/java/com/fjlima/helpdesk/api/repository/TicketRepository.java
+++ src/main/java/com/fjlima/helpdesk/api/repository/TicketRepository.java
@@ -10,14 +10,14 @@ public interface TicketRepository extends MongoRepository<Ticket, String> {
 
 	Page<Ticket> findByUserIdOrderByDateDesc(Pageable pages, String userId);
 
-	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusIgnoreCaseContainingAndPriorityIgnoreCaseContainingOrderByDateDesc(
-			String title, String status, String priority, Pageable pages);
+	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityOrderByDateDesc(String title, String status,
+			String priority, Pageable pages);
 
-	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusIgnoreCaseContainingAndPriorityIgnoreCaseContainingAndUserIdOrderByDateDesc(
-			String title, String status, String priority, Pageable pages);
+	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityAndUserIdOrderByDateDesc(String title,
+			String status, String priority, Pageable pages);
 
-	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusIgnoreCaseContainingAndPriorityIgnoreCaseContainingAndAssignedUserOrderByDateDesc(
-			String title, String status, String priority, Pageable pages);
+	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityAndAssignedUserOrderByDateDesc(String title,
+			String status, String priority, Pageable pages);
 
 	Page<Ticket> findByNumber(Integer number, Pageable pages);
 
